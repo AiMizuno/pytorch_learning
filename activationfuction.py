52d914d07f6ecdd815a502a71971858af1e42baf
Ai_Mizuno <lizhh43@mail2.sysu.edu.cn>
2019-02-26 19:28:42 +0800

    regression tutorial

--- activationfuction.py
+++ activationfuction.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 from torch.autograd import Variable
 
-x = torch.linesapce(-5, 5, 200)
+x = torch.linspace(-5, 5, 20)
 x = Variable(x)
 
 x_np = x.data.numpy()   # 换成 numpy array, 出图时用
@@ -14,3 +15,26 @@ y_sigmoid = F.sigmoid(x).data.numpy()
 y_tanh = F.tanh(x).data.numpy()
 y_softplus = F.softplus(x).data.numpy()
 # y_softmax = F.softmax(x)  softmax 比较特殊, 不能直接显示, 不过他是关于概率的, 用于分类
+
+plt.figure(1, figsize=(8, 6))
+plt.subplot(221)
+plt.plot(x_np, y_relu, c='red', label='relu')
+plt.ylim((-1, 5))
+plt.legend(loc='best')
+
+plt.subplot(222)
+plt.plot(x_np, y_sigmoid, c='red', label='sigmoid')
+plt.ylim((-0.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(223)
+plt.plot(x_np, y_tanh, c='red', label='tanh')
+plt.ylim((-1.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(224)
+plt.plot(x_np, y_softplus, c='red', label='softplus')
+plt.ylim((-0.2, 6))
+plt.legend(loc='best')
+
+plt.show()
